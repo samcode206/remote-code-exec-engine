64757965037d78907a3b0ca7a10d6bd719b02e16
Sameem Hadi <sameem@Sameems-MacBook-Pro.local>
2021-07-03 15:45:21 -0700

    added bull for queue

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,37 @@
 import Fastify, { FastifyInstance } from 'fastify';
+import Queue from 'bull';
+
+
+const codeQueue = new Queue("run-code", {
+  redis:{
+    port: 6379,
+    host: "127.0.0.1",
+  }
+});
+
+
 
 
 const server: FastifyInstance = Fastify();
 
+
+
+
+interface CodeAttrs {
+  code: string;
+  problem: string; 
+}
+
+server.post<{Body: CodeAttrs}>('/code', (request, reply) => {
+  const {body} = request; 
+   
+  reply.send({ack: true});
+
+}); 
+
+
+
+//  entry to our api route (ignore for now maybe add docs later)
 server.route({
   method: "GET",
   url: '/',
@@ -21,23 +50,6 @@ server.route({
     return reply.send({message: "hello"});
   }
 });
-
-
-
-
-interface CodeAttrs {
-  code: string;
-  problem: string; 
-}
-
-server.post<{Body: CodeAttrs}>('/code', (request, reply) => {
-  const {body} = request; 
-  
-  console.log(body.code);
-
-  reply.send({ack: true});
-
-}); 
 
 
 
