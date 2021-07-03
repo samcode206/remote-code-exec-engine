5649af0ec43dd1f1cce100d9d6fa3bef4b60f254
Sameem Hadi <sameem@Sameems-MacBook-Pro.local>
2021-07-03 15:26:06 -0700

    route created for code

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,41 +1,60 @@
-import Fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify'
-// import { Server, IncomingMessage, ServerResponse } from 'http';
+import Fastify, { FastifyInstance } from 'fastify';
+
 
 const server: FastifyInstance = Fastify();
 
-const opts: RouteShorthandOptions = {
+server.route({
+  method: "GET",
+  url: '/',
   schema: {
+    // the response needs to be an object with an `hello` property of type 'string'
     response: {
       200: {
         type: 'object',
         properties: {
-          message: {
-            type: 'string',
-          }
+          message: {type: "string"}
         }
       }
     }
+  },
+  handler: async (request, reply) => {
+    return reply.send({message: "hello"});
   }
-}
-
-server.get('/', opts, async (request, reply) => {
-    return reply.send({message: 'it worked!'})
 });
 
 
 
+
+interface CodeAttrs {
+  code: string;
+  problem: string; 
+}
+
+server.post<{Body: CodeAttrs}>('/code', (request, reply) => {
+  const {body} = request; 
+  
+  console.log(body.code);
+
+  reply.send({ack: true});
+
+}); 
+
+
+
+// start the server iife 
 (async () => {
     try {
-      await server.listen(3000)
+      await server.listen(5000)
   
-      const address = server.server.address()
-      const port = typeof address === 'string' ? address : address?.port
+      const address = server.server.address();
+      const port = typeof address === 'string' ? address : address?.port;
   
       console.log("listening on port", port);
       
-    } catch (err) {
+    } 
+    catch (err) {
       server.log.error(err)
       process.exit(1)
-    }
+    };
   })();
 
